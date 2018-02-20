97a6f28470d3b833b3a18361a2a8cd54e73bdb09
mkivikin <mkivikin@users.noreply.github.com>
2018-02-20 15:25:18 +0200

    Lisatud joonistamise funktsioon, skaala

--- Main.java
+++ Main.java
@@ -25,18 +25,80 @@ public class Main{
 			Players.add(p);
 			rida = bi.readLine();
 		}
-		System.out.println(getAvg(Players));
+		System.out.println(getAvg(Players, "SG"));
+		Joonesta(Players, "SG", "PG");
+		
+		
 			
 	}
 	//Funktsioon keskmise leidmiseks
-	public static float getAvg(ArrayList<Player> Players) {
+	public static float getAvg(ArrayList<Player> Players, String position) {
 			float sum = 0;
 			int index = 0;
+			int count = 0;
 			for(Player k : Players){
-				sum = sum + ((Players.get(index)).getPoints());
+				//Kui mängija objekti positsioon on võrdne kasutaja sisestatud positsiooniga
+				if((Players.get(index)).getPosition().equals(position)) {
+					System.out.println(Players.get(index).getPoints());
+					sum = sum + ((Players.get(index)).getPoints());
+					count++;
+				}
 				index++;
 			}
-			float avg = sum/(index+1);
+			float avg = sum/(count);
 			return avg;
 		}
+	public static void Joonesta(ArrayList<Player> Players, String pos1, String pos2) throws Exception{
+		int imgWidth = 500;
+		int imgHeight = 500;
+		BufferedImage bi = new BufferedImage(imgWidth, imgHeight, BufferedImage.TYPE_INT_RGB);
+		Graphics g = bi.createGraphics();
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, 500, 500);
+		int index = 0;
+		int x1 = 25;
+		int dotSize = 3;
+		int ypos = 0;
+		//Joonestab esimese positsiooni mängijate punktid
+		for (Player p : Players) {
+			if ((Players.get(index)).getPosition().equals(pos1)) {
+				g.setColor(Color.BLUE);
+				int points = Math.round(Players.get(index).getPoints())*10;
+				ypos = imgHeight-points;
+				g.fillOval(x1, ypos, dotSize, dotSize);
+				x1 = x1+25;
+			}
+			if ((Players.get(index)).getPosition().equals(pos2)) {
+				g.setColor(Color.RED);
+				int points = Math.round(Players.get(index).getPoints())*10;
+				ypos = imgHeight-points;
+				g.fillOval(x1, ypos, dotSize, dotSize);
+				x1 = x1+25;
+			}
+			index++;
+		}
+		ypos = imgHeight-(Math.round(getAvg(Players, pos1))*10);
+		//Joonestab sirge mis on esimese positsiooni keskmine punktide arv
+		g.setColor(Color.BLUE);
+		g.drawLine(0, ypos, 500, ypos);
+		ypos = imgHeight-(Math.round(getAvg(Players, pos2))*10);
+		//Joonestab sirge mis on võrdne teise positsiooni keskmise punktide arvuga
+		g.setColor(Color.RED);
+		g.drawLine(0, ypos, 500, ypos);
+		//Joonestan mingid graafiku skaala
+		g.setColor(Color.BLACK);
+		index = 0;
+		ypos = imgWidth-50;
+		int points = 0;
+		while (index<(imgWidth/50)){
+			g.drawLine(0, ypos, 10, ypos);
+			ypos = ypos-5; 
+			g.drawString(String.valueOf(points), 3, ypos);
+			points = points+5;
+			ypos= ypos - 40;
+			index++;
+		}
+		ImageIO.write(bi, "png", new File("keskmised.png"));
+	}
+		
 }
