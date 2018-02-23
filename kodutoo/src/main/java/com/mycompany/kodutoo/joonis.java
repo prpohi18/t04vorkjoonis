3e5cb5e3036c6b6e57987e33aa1f1f13a44c9a10
Kärt Nigols <kart.nigols@gmail.com>
2018-02-23 15:35:24 +0200

    Kodutöö nr.4

--- kodutoo/src/main/java/com/mycompany/kodutoo/joonis.java
+++ kodutoo/src/main/java/com/mycompany/kodutoo/joonis.java
@@ -4,12 +4,13 @@ import javax.imageio.*;
 import java.awt.*;
 import java.net.*;
 import java.io.*;
+
 public class joonis{
     public static void main(String[] arg) throws Exception{
-        BufferedImage bi=new BufferedImage(400, 300, BufferedImage.TYPE_INT_RGB);
+        BufferedImage bi=new BufferedImage(500, 500, BufferedImage.TYPE_INT_RGB);
         Graphics g=bi.createGraphics();
         g.setColor(Color.BLACK);
-        g.fillRect(0, 0, 1000, 1000);
+        g.fillRect(0, 0, 500, 500);
         String aadress="http://www.tlu.ee/~jaagup/veeb1/loomad.txt";
         BufferedReader br=new BufferedReader(new InputStreamReader(
           new URL(aadress).openStream()));
@@ -19,56 +20,24 @@ public class joonis{
         //muutujad:
         String mass=new String("");
         String kõrg=new String("");
-        int kasskeskmass=0;
-        int save1=0;
-        int save2=0;
-        int save3=0;
-        int save4=0;
-        int koerkeskmass=0;
-        int i=0;
-        int b=0;
-        int kasskeskkõrg=0;
-        int koerkeskkõrg=0;
-        
-        
+
         while(rida!=null){
             String[] m=rida.split(",");
             if(m[0].equals("kass")){
                 g.setColor(Color.orange);
                 mass= m[1];
                 kõrg=m[2];
-                save1=save1+Integer.parseInt(m[1]);//masside summa 
-                save3=save3+Integer.parseInt(m[2]);//kõrguste summa
-                i++;//mitu kassi oli
           
             }
             else{
                 g.setColor(Color.white);
                 mass= m[1];
                 kõrg=m[2];
-                save2=save2+Integer.parseInt(m[1]);//masside summa
-                save4=save4+Integer.parseInt(m[2]);//kõrguste  summa
-                b++;
             }
-            
-            //joonised
-            //g.fillRect(Integer.parseInt(m[1])/30, 300-Integer.parseInt(m[2])*2, 5, 5);
-            g.drawString(mass,Integer.parseInt(m[1]),Integer.parseInt(m[1]));//string,int x, int y
-            g.drawString(kõrg,Integer.parseInt(m[2]),Integer.parseInt(m[2]));//string intx int y
+            g.drawString(mass,Integer.parseInt(m[1])/10,Integer.parseInt(m[1])/10);//string,int x, int y
+            g.drawString(kõrg,Integer.parseInt(m[2])/10,Integer.parseInt(m[2])/10);//string intx int y
             rida=br.readLine();
         }
-        kasskeskkõrg=save3/i;
-        kasskeskmass=save1/i;
-        koerkeskmass=save2/b;
-        koerkeskkõrg=save4/b;
-        g.setColor(Color.red); 
-        g.drawRect(kasskeskmass,kasskeskmass,10,10);
-        g.setColor(Color.gray);
-        g.drawRect(koerkeskmass,koerkeskmass,10,10);
-        g.setColor(Color.blue);
-        g.drawOval(kasskeskkõrg, kasskeskkõrg, 10, 10);
-        g.setColor(Color.pink);
-        g.drawOval(koerkeskkõrg, koerkeskkõrg, 10, 10);
         ImageIO.write(bi, "png", new File("loomad.png"));
     }
 }
--- kodutoo/src/main/java/com/mycompany/kodutoo/vork.java
+++ kodutoo/src/main/java/com/mycompany/kodutoo/vork.java
@@ -18,9 +18,25 @@ public class vork {
         int koeradesumma=0;
         int b=0;
         int massidesumma=0;
+        int koerkõrg=0;
+        int kassikõrg=0;
+        int koerkeskkõrg=0;
+        int kasskeskkõrg=0;
+        int kasskeskmass=0;
+        int koerkeskmass=0;
+        int suurimkass=0;
+        int suurimkass1=0;
+        int suurimkoer=0;
+        int suurimkoer1=0;
+        int väikseimkass=0;
+        int väikseimkass1=1000;
+        int väikseimkoer=0;
+        int väikseimkoer1=12000;
+                
         while(rida!=null){
             System.out.println(rida);
             String[] m=rida.split(",");
+            //Sisesta kaks arvu koerte massideks ning kolm arvu kasside massideks. Näita, kummast liigist loomade masside summa on suurem.
             if(m[0].equals("kass") && i!=3){
                 kassidemassidesumma+=Integer.parseInt(m[1]);
                 i++;
@@ -28,7 +44,34 @@ public class vork {
             if(m[0].equals("koer")&& b!=2){
                 b++;
                 koeradesumma+=Integer.parseInt(m[1]);
-            }  
+            }
+            //Lisa funktsioonid suurima, vähima ja aritmeetilise keskmise massi väärtuse leidmiseks.
+            if(m[0].equals("kass")){
+                suurimkass= Integer.parseInt(m[1]);
+                väikseimkass=Integer.parseInt(m[1]);
+                kassidemassidesumma+=Integer.parseInt(m[1]);
+                kassikõrg+=Integer.parseInt(m[2]);
+                i++;
+                if(suurimkass1<suurimkass){
+                    suurimkass1= suurimkass;}   
+             }
+                if(väikseimkass1>väikseimkass){
+                    väikseimkass1=väikseimkass;
+                }
+            if(m[0].equals("koer")){
+                suurimkoer=Integer.parseInt(m[1]);
+                väikseimkoer=Integer.parseInt(m[1]);
+                b++;
+                koeradesumma+=Integer.parseInt(m[1]);
+                koerkõrg+=Integer.parseInt(m[2]);
+                if(suurimkoer1<suurimkoer){
+                    suurimkoer1=suurimkoer;
+                }
+                if(väikseimkoer1>väikseimkoer){
+                    väikseimkoer1=väikseimkoer;
+                }
+            }
+            
             massidesumma=koeradesumma+kassidemassidesumma;
             rida=br.readLine();
             
@@ -36,6 +79,10 @@ public class vork {
         System.out.println("Kassimasside summa on :"+kassidemassidesumma);
         System.out.println("Koertemasside summa on:"+koeradesumma);
         System.out.println("Masside summa on:"+massidesumma);
+        System.out.println("Suurima kassi mass on:"+suurimkass1);
+        System.out.println("Suurima koera mass on:"+suurimkoer1);
+        System.out.println("Väikseim kass on:"+väikseimkass1);
+        System.out.println("Väikseim koer on:"+väikseimkoer1);
         if(koeradesumma<kassidemassidesumma){
             System.out.println("Kasside masside summa on suurem.");
         }else{
