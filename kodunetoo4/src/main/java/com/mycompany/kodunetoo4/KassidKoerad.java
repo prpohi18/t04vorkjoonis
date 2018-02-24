cab7c327d78693ac58426494ca88f8c7fb95d868
Kertu Mikk <kertumik@tlu.ee>
2018-02-24 15:51:56 +0200

    kõik peale keskmise joonisele kirjutamine olemas

--- kodunetoo4/src/main/java/com/mycompany/kodunetoo4/KassidKoerad.java
+++ kodunetoo4/src/main/java/com/mycompany/kodunetoo4/KassidKoerad.java
@@ -19,134 +19,79 @@ import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
 
 public class KassidKoerad {
-    java.util.List<DescriptiveStatistics> kassidMass;
-    java.util.List<DescriptiveStatistics> kassidKorgus;
-    java.util.List<DescriptiveStatistics> koeradMass;
-    java.util.List<DescriptiveStatistics> koeradKorgus;
-    
-  
-     
-    
-    
-    
-    
-    
-    
     public static void main(String[] arg) throws Exception {
-            
-            ArrayList<String> kassidMass = new ArrayList<>();
-            ArrayList<String> kassidKorgus = new ArrayList<>();
-            ArrayList<String> koeradMass = new ArrayList<>();
-            ArrayList<String> koeradKorgus = new ArrayList<>();
-             
-             
-            BufferedImage bi=new BufferedImage(400, 300, BufferedImage.TYPE_INT_RGB);
-            Graphics g=bi.createGraphics();
-            g.setColor(Color.WHITE);
-            g.fillRect(0, 0, 400, 300);
-
-            
-           
-            try {   
+        ArrayList<String> kassidMass = new ArrayList<>();
+        ArrayList<String> kassidKorgus = new ArrayList<>();
+        ArrayList<String> koeradMass = new ArrayList<>();
+        ArrayList<String> koeradKorgus = new ArrayList<>();
+        BufferedImage bi=new BufferedImage(400, 300, BufferedImage.TYPE_INT_RGB);
+        Graphics g=bi.createGraphics();
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, 400, 300);
+        try {   
                 String address= "http://www.tlu.ee/~jaagup/veeb1/loomad.txt";
                 BufferedReader br=new BufferedReader( new InputStreamReader(new URL(address).openStream()));
                 String rida = br.readLine();
-                
                 rida = br.readLine();
                 String [] test = new String[rida.split(",").length];
-            
+
             while (rida != null) {
                 String[] m = rida.split(",");
-                
-                   
-                    if("koer".equals(m[0])){
-                        
-                        koeradMass.add(m[1]);
-                        
-                        koeradKorgus.add(m[2]);
-                        
-                        
-                        
-                        g.setColor(Color.gray);
-                        g.fillRect(Integer.parseInt(m[1])/30, 300-Integer.parseInt(m[2])*2, 5, 5);
-                        
-                    }else if("kass".equals(m[0])){
-                        
-                        kassidMass.add(m[1]);
-                        kassidKorgus.add(m[2]);
-                        
-                        
-                        
-                        g.setColor(Color.red);
-                        g.fillRect(Integer.parseInt(m[1])/30, 300-Integer.parseInt(m[2])*2, 5, 5);
-                    }
-                    
-                    
-                    rida = br.readLine();
-                    
-                
-                
+                if("koer".equals(m[0])){
+                    koeradMass.add(m[1]);
+                    koeradKorgus.add(m[2]);
+                    g.setColor(Color.gray);
+                    g.fillRect(Integer.parseInt(m[1])/30, 300-Integer.parseInt(m[2])*2, 5, 5);
+                }else if("kass".equals(m[0])){
+                    kassidMass.add(m[1]);
+                    kassidKorgus.add(m[2]);
+                    g.setColor(Color.red);
+                    g.fillRect(Integer.parseInt(m[1])/30, 300-Integer.parseInt(m[2])*2, 5, 5);
+                }
+                rida = br.readLine();
                 ImageIO.write(bi, "png", new File("loomad/loomad.png"));
-              
-                
             }
-           
-            
+            g.setColor(Color.red);
+            g.drawString("Kass = punane",10,25);
+            g.drawString("Koer = hall",10,35);
+            ImageIO.write(bi, "png", new File("loomad/loomad.png"));
             br.close();
-               
-        
- 
-             try{
-                    PrintWriter pw=new PrintWriter(new FileWriter("loomad/kassid.txt"));
-                    pw.println("Kasside massid");
-                    pw.println(String.join(", ",kassidMass));
-                    pw.println("Kasside korgused");
-                    pw.println(String.join(", ", kassidKorgus));
-                    pw.close();
-
+            try{
+                PrintWriter pw=new PrintWriter(new FileWriter("loomad/kassid.txt"));
+                pw.println("Kasside massid");
+                pw.println(String.join(", ",kassidMass));
+                pw.println("Kasside korgused");
+                pw.println(String.join(", ", kassidKorgus));
+                pw.close();
                 } catch(Exception ex){
                     ex.printStackTrace();
                 } 
-             
-                try{
-                    PrintWriter pw=new PrintWriter(new FileWriter("loomad/koerad.txt"));
-                    pw.println("Koerte massid");
-                    pw.println(String.join(", ",koeradMass ));
-                    pw.println("Koerte korgused");
-                    pw.println(String.join(", ",koeradKorgus ));
-                    pw.close();
-
-                } catch(Exception ex){
+            try{
+                PrintWriter pw=new PrintWriter(new FileWriter("loomad/koerad.txt"));
+                pw.println("Koerte massid");
+                pw.println(String.join(", ",koeradMass ));
+                pw.println("Koerte korgused");
+                pw.println(String.join(", ",koeradKorgus ));
+                pw.close();
+            } catch(Exception ex){
                     ex.printStackTrace();
-                } 
-            
-            
-            
-            
+            } 
             /*System.out.println("Kasside massid "+kassidMass);
             System.out.println("Kasside kõrgused "+kassidKorgus);
             System.out.println("Koerte massid "+koeradMass);
             System.out.println("Koerte kõrgused "+koeradKorgus);*/
             arvutamine a1 = new arvutamine();
-            
-            
-            
-            
-            
-            
-            
+            System.out.println();
         } catch (Exception ex) {
-                    ex.printStackTrace();}
-        
-        
-       
-}   
+            ex.printStackTrace();
+        }
+    }   
 }
 //http://www.tlu.ee/~jaagup/veeb1/loomad.txt
 
 /*
-Kasside masside keskmine: 1300
-Kasside kõrguste kõrguste keskmine: 1300
+Kasside masside keskmine: 1200
+Kasside kõrguste kõrguste keskmine: 27
 Kasside max mass 1200 ja kõrgus 28
 Kasside min mass 800 ja kõrgus 20
 Koerte masside keskmine: 3000
--- kodunetoo4/src/main/java/com/mycompany/kodunetoo4/arvutamine.java
+++ kodunetoo4/src/main/java/com/mycompany/kodunetoo4/arvutamine.java
@@ -1,19 +1,19 @@
 
 package com.mycompany.kodunetoo4;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 public final class arvutamine {
-   public arvutamine() throws IOException 
-   {
-        
-           System.out.println(arvuta());
-           
-       
-   }  
-       
+    public arvutamine() throws IOException 
+    {
+        arvuta();
+    }  
     public String arvuta() throws IOException{   
         int kassidemassidesumma=0;
         int koertemassidesumma=0;
@@ -31,40 +31,35 @@ public final class arvutamine {
         int KKK = 0;
         int PMK = 0;
         int PKK = 0;
-        
         try {
-            BufferedReader br = new BufferedReader(new FileReader("loomad\\kassid.txt"));
-            String rida = br.readLine();
-            rida = br.readLine();
-            String[] m = rida.split(", ");
-            for(int i=0; i<m.length; i++){
-                kassidemassidesumma = kassidemassidesumma +(Integer.parseInt (m[i]));
-                if (i< kassMinMass){
-                    kassMinMass = (Integer.parseInt( m[i]));
-                    
+            try (BufferedReader br = new BufferedReader(new FileReader("loomad\\kassid.txt"))) {
+                String rida = br.readLine();
+                rida = br.readLine();
+                String[] m = rida.split(", ");
+                for (int i=0; i<m.length; i++) {
+                    kassidemassidesumma = kassidemassidesumma + (Integer.parseInt(m[i]));
+                    if (i< kassMinMass){
+                        kassMinMass = (Integer.parseInt(m[i]));
+                    }
+                    if (i > kassMaxMass){
+                        kassMaxMass = (Integer.parseInt(m[i]));
+                    }
                 }
-                if (i > kassMaxMass){
-                    kassMaxMass = (Integer.parseInt( m[i]));
-                    
+                rida = br.readLine();
+                rida = br.readLine();
+                String[] k = rida.split(", ");
+                for(int i=0; i<k.length; i++){
+                    kassidekorgustesumma = kassidekorgustesumma +(Integer.parseInt (k[i]));
+                    if (i< kassMinKorgus){
+                        kassMinKorgus =(Integer.parseInt(k[i]));
+                    }
+                    if (i > kassMaxKorgus){
+                        kassMaxKorgus = (Integer.parseInt(k[i]));
+                    }
                 }
+                KMK = kassidemassidesumma / m.length;
+                KKK = kassidekorgustesumma / k.length;
             }
-            rida = br.readLine();
-            rida = br.readLine();
-            String[] k = rida.split(", ");
-             for(int i=0; i<k.length; i++){
-                kassidekorgustesumma = kassidekorgustesumma +(Integer.parseInt (m[i]));
-                if (i< kassMinKorgus){
-                    kassMinKorgus = (Integer.parseInt( k[i]));
-                    
-                }
-                if (i > kassMaxKorgus){
-                    kassMaxKorgus = (Integer.parseInt( k[i]));
-                    
-                }
-            }
-        KMK = kassidemassidesumma / m.length;
-        KKK = kassidekorgustesumma / k.length;
-        br.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(arvutamine.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -77,36 +72,37 @@ public final class arvutamine {
                     koertemassidesumma = koertemassidesumma + (Integer.parseInt(m[1]));
                     if (i< koerMinMass){
                         koerMinMass = (Integer.parseInt( m[i]));
-                    
                     }
                     if (i > koerMaxMass){
                         koerMaxMass = (Integer.parseInt( m[i]));
-
                     }
                 }
                 rida = br.readLine();
                 rida = br.readLine();
                 String[] k = rida.split(", ");
                 for(int i=0; i<k.length; i++){
-                    koertekorgustesumma = koertekorgustesumma +(Integer.parseInt (m[i]));
+                    koertekorgustesumma = koertekorgustesumma +(Integer.parseInt (k[i]));
                     if (i< koerMinKorgus){
                         koerMinKorgus =(Integer.parseInt( k[i]));
-                    
                     }
                     if (i > koerMaxKorgus){
                         koerMaxKorgus = (Integer.parseInt( k[i]));
-
                     }
                 }
-                PMK = koertemassidesumma / m.length;
-                PKK = koertekorgustesumma / k.length;
+                br.close();                     
             }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(arvutamine.class.getName()).log(Level.SEVERE, null, ex);
         }
-        String vastus = "Kasside masside keskmine: "+KKK+"\nKasside kõrguste kõrguste keskmine: "+KKK+"\nKasside max mass "+kassMaxMass+" ja kõrgus "+kassMaxKorgus+"\n"
-                + "Kasside min mass "+kassMinMass+" ja kõrgus "+kassMinKorgus+"\nKoerte masside keskmine: "+PMK+"\nKoerte kõrguste keskmine: "+PKK+"\nKoerte max mass "+koerMaxMass+" ja kõrgus "+koerMaxKorgus+"\n"
-          + "Koerte min mass "+koerMinMass+" ja kõrgus "+koerMinKorgus;
+        String vastus = "Kasside masside keskmine: "+KMK+"\nKasside korguste keskmine: "+KKK+"\nKasside max mass "+kassMaxMass+" ja korgus "+kassMaxKorgus+"\n"
+        + "Kasside min mass "+kassMinMass+" ja korgus "+kassMinKorgus+"\nKoerte masside keskmine: "+PMK+"\nKoerte korguste keskmine: "+PKK+"\nKoerte max mass "+koerMaxMass+" ja korgus "+koerMaxKorgus+"\n"
+        + "Koerte min mass "+koerMinMass+" ja korgus "+koerMinKorgus;
+       try{
+            try (PrintWriter pw = new PrintWriter(new FileWriter("loomad/arvutused.txt"))) {
+                pw.println(vastus);
+            }
+        } catch(IOException ex){
+        } 
         return vastus;
     } 
 }
